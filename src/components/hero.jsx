552c307791100cf89a07dcfa6533f848327d5f9d
ray-pasino <ray.pasino@gmail.com>
2024-09-24 14:24:34 +0000

    allowed gradeint on blur

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -19,25 +19,13 @@ const Hero = () => {
   return (
     <div className='hero h-screen'>
       {!imgLoaded && 
-        // <Blurhash
-        //   hash='LFIpMO1G00}a1GRj%NOr00-V.mMw'
-        //   width={'100%'}
-        //   height={'100%'}
-        //   resolutionX={32}
-        //   resolutionY={32}
-        //   punch={1}
-        //   className='absolute top-0 left-0 z-10'
-        //   />
        <img src="./coverblur.png" className='backgroundImage absolute z-0 h-screen w-full object-cover'/>
-
       }
 
       {imgLoaded &&
-      <>
        <img src="./coverphoto1.jpeg" className='backgroundImage absolute z-0 h-screen w-full object-cover'/>
-       <div className="gradient absolute z-20"></div> 
-      </>
       }
+      <div className="gradient absolute z-20"></div> 
       <Nav/>
       <Info/>
     </div>
