1aba17b3f195110427766c2fae45618f26b99420
ray-pasino <ray.pasino@gmail.com>
2024-09-24 14:37:24 +0000

    reverted sizes

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -13,8 +13,8 @@ const Nav = () => {
   return (
     <div className="nav flex justify-between mx-2 sm:mx-8 lg:mx-16 relative">
       <div className="logo flex z-30 mt-4 lg:mt-0">
-        <img src={assets.logo} alt="REGENCY LOGO" className="h-14 sm:h-24" />
-        <span className="title text-white mt-2 sm:mt-7 text-2xl sm:text-2xl font-bold">REGENCY</span>
+        <img src={assets.logo} alt="REGENCY LOGO" className="h-12 sm:h-24" />
+        <span className="title text-white mt-2 sm:mt-7 text-xl sm:text-2xl font-bold">REGENCY</span>
       </div>
       <div className="nav-links z-30 mt-4 lg:mt-0">
         <FontAwesomeIcon
