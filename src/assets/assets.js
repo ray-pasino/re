e9bf28390e374b9bbd24339ca59f647710827558
ray-pasino <pasinoray@gmail.com>
2024-10-27 18:37:42 +0000

    styled sec2

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,9 +1,14 @@
 import logo from './logo.png'
 import caretdown from './caretdown.png'
-
+import exploreIcon from './explore-icon.png'
+import badgeIcon from './badge-icon.png'
+import aboutIcon from './about-icon.png'
 
 
 export const assets = {
     logo,
-    caretdown
+    caretdown,
+    exploreIcon,
+    badgeIcon,
+    aboutIcon
 }
--- src/components/OnlineInfoSection.jsx
+++ src/components/OnlineInfoSection.jsx
@@ -1,5 +1,14 @@
 import React,{useState, useEffect} from 'react'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-creative';
+
+import { EffectCreative, Pagination} from 'swiper/modules';
+import { assets } from '../assets/assets';
+
+
 const OnlineInfoSection = () => {
 
     const [backgroundLoaded, setBackgroundLoaded] = useState(false)
@@ -22,21 +31,66 @@ const OnlineInfoSection = () => {
         !backgroundLoaded ?
        <img src="./ladyblur.png" className='backgroundImage absolute z-0 h-screen w-full object-cover'/>
             :
-       <img src="./lady.jpg" className='backgroundImage absolute z-0 h-screen w-full object-cover'/>
+       <img src="./lady.jpg" className='backgroundImage absolute z-0 h-screen w-full object-cover opacity-[40%]'/>
         }
 
         <div className="gradient-2 absolute z-20 sm:hidden"></div>
-        <div className="sec-info absolute z-30 sm:hidden">
-            <p>
-                <span>Always On The Move?</span>
-                <span>So Are We.</span>
+        <div className="sec-info absolute z-30 sm:hidden mt-[2vh] text-white flex flex-col m-[34px]">
+            <p className='text-[30px] font-bold h-[20vh]'>
+                <span className='w-screen mr-2'>Always On The Move??</span>
+                <span className='w-screen'>So Are We.</span>
             </p>
 
-            <p>
+            <p className='font-semibold text-[16px] h-[40vh]'>
             Unlike traditional universities offering online classes, we are a mobile university tailored specifically for busy adults.
-             Our innovative platform allows you to complete and submit coursework directly from your smartphone. Yes, it's that convenient.
+             Our innovative platform allows you to complete and submit coursework directly from your smartphone. Yes, it's that convenient.<br/>
+             <a className='text-blue-600 border-b border-blue-600' href='#'>Learn More</a>
             </p>
+ 
+        
+        <Swiper
+        grabCursor={true}
+        effect={'creative'}
+        creativeEffect={{
+          prev: {
+            shadow: true,
+            translate: [0, 0, -400],
+          },
+          next: {
+            translate: ['100%', 0, 0],
+          },
+        }}
+        pagination={{
+            clickable: true,
+          }}
+        modules={[EffectCreative, Pagination]}
+        className="mySwiper h-[38vh] w-[85vw] mb-0"
+      >
+        <SwiperSlide>
+            <div className='card rounded-md flex flex-col items-center shadow-lg'>
+                <img src={assets.exploreIcon} alt="explore" className='w-full h-full object-contain p-8'/>
+                <p className='font-bold text-2xl p-4'>EXPLORE PROGRAMS</p>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className='card rounded-md flex flex-col items-center shadow-lg'>
+            <img src={assets.badgeIcon} alt="experience" className='w-full h-full object-contain p-8'/>
+            <p className='font-bold text-2xl p-4'>THE REGENCY EXPERIENCE</p>
+            </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className='card rounded-md flex flex-col items-center shadow-lg'>
+            <img src={assets.aboutIcon} alt="about"  className='w-full h-full object-contain p-8'/>
+            <p className='font-bold text-2xl p-4'>ABOUT ADMISSION</p>
+            </div>
+        </SwiperSlide>
+    
+      </Swiper>
+            
         </div>
+
+
+        
     </div>
   )
 }
